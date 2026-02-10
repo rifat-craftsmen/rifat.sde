c9b03cddbe6249e3ca7a0825b8da5602ae54950d
rifat-craftsmen <rifat.ahmed@craftsmensoftware.com>
2026-02-10 09:04:34 +0600

    authroutes.ts updated

--- Meal_Planner_System/backend/src/routes/authRoutes.ts
+++ Meal_Planner_System/backend/src/routes/authRoutes.ts
@@ -7,7 +7,7 @@ import rateLimit from 'express-rate-limit';
 const router = Router();
 
 const loginLimiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
+  windowMs: 10 * 60 * 1000, // 10 minutes
   max: 5, // 5 attempts
   message: 'Too many login attempts, please try again later',
 });
