6c345abade74111aa8facb41064dc77d8d60c481
rifat-craftsmen <rifat.ahmed@craftsmensoftware.com>
2026-02-15 14:37:44 +0600

    single page webapp

---
+++ react-webapp/src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+export default function Navbar() {
+  return (
+    <nav aria-label="Main navigation">
+      <ul>
+        <li><a href="#home">Home</a></li>
+        <li><a href="#flex">Flex Section</a></li>
+        <li><a href="#grid">Grid Section</a></li>
+        <li><a href="#contact">Footer</a></li>
+      </ul>
+    </nav>
+  );
+}
