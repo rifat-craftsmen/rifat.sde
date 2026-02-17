d53cbe3b7d15d019452ef1dcb09048b355b1232c
rifat-craftsmen <rifat.ahmed@craftsmensoftware.com>
2026-02-17 11:27:03 +0600

    add README.md file for MHP

--- Meal_Planner_System/backend/dist/utils/jwt.js
+++ Meal_Planner_System/backend/dist/utils/jwt.js
@@ -1,10 +1,5 @@
 import jwt from 'jsonwebtoken';
 const JWT_SECRET = process.env.JWT_SECRET;
-
-if (!JWT_SECRET) {
-    throw new Error("JWT_SECRET is missing! Please define it in your .env file.");
-}
-
 export const generateToken = (payload) => {
     return jwt.sign(payload, JWT_SECRET, { expiresIn: '7d' });
 };
