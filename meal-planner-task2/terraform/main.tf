15cfb5f4ce93f0f801d290ec994dda71093d0d93
rifat-craftsmen <rifat.ahmed@craftsmensoftware.com>
2026-04-16 17:38:34 +0600

    create a s3 bucket for storing .tfstate file

--- meal-planner-task2/terraform/main.tf
+++ meal-planner-task2/terraform/main.tf
@@ -5,6 +5,12 @@ terraform {
       version = "~> 5.0"
     }
   }
+
+  backend "s3" {
+    bucket = "trainee-rifat-mhpv2-tfstate"
+    key    = "meal-planner-task2/terraform.tfstate"
+    region = "ap-southeast-1"
+  }
 }
 
 provider "aws" {
